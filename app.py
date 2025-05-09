041a9985d8dcecddfb5e6df8b24032f7e0eee056
Arthur-Couto <arthurdocouto@ufrj.br>
2025-05-09 15:13:47 -0300

    criando dicionários

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 
-restaurantes = ['Pizzaria do Zé', 'Sushi do Jão']
+restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False}, 
+                {'nome':'Pizza Superma', 'categoria':'Pizza', 'ativo':True},
+                {'nome':'Cantina', 'categoria':'Italiano', 'ativo':False},
+]
 
 def exibir_nome_do_programa():
     print("""
@@ -46,7 +49,10 @@ def listar_restaurantes():
     exibir_subtitulo('Lista de restaurantes')
     
     for restaurante in restaurantes:
-        print(f'.{restaurante}')
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+        print(f'- {nome_restaurante} | {categoria} | {ativo}' )
 
     voltar_ao_menu_principal()
 
